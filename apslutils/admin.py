cee761b25b3ee0534f38d37adee6ac98a8acb375
Daniel Sastre <dsastre@apsl.net>
2014-03-07 09:08:19 +0100

    Añadido soporte para usar DateTimeField y DateField indistintamente.

--- apslutils/admin.py
+++ apslutils/admin.py
@@ -5,6 +5,7 @@
 import datetime
 
 from django.contrib import admin
+from django.db.models.fields import DateTimeField
 
 
 class ComboBoxFiltro(admin.RelatedFieldListFilter):
@@ -26,7 +27,7 @@ class ComboBoxCharFiltro(admin.AllValuesFieldListFilter):
 class RangoFechasFiltro(admin.SimpleListFilter):
     u"""
     Filtro para rangos de fechas. Además se puede escoger entre distintos
-    campos del modelo. Por ejemplo filtrar por fecha de creación o 
+    campos del modelo. Por ejemplo filtrar por fecha de creación o
     fecha de modificación.
     """
 
@@ -36,6 +37,10 @@ class RangoFechasFiltro(admin.SimpleListFilter):
 
     # Tipos de filtraje
     filtro_tipos = ()
+
+    def __init__(self, request, params, model, model_admin):
+        super(RangoFechasFiltro, self).__init__(request, params, model, model_admin)
+        self.model = model
 
     def value(self):
         valor = self.used_parameters.get(self.parameter_name, None)
@@ -48,14 +53,13 @@ class RangoFechasFiltro(admin.SimpleListFilter):
         return self.filtro_tipos
 
     def queryset(self, request, queryset):
-
         filtro = request.GET.get(self.parameter_name)
         if filtro:
             desde, hasta = filtro.split('|')[1:]
-            
+
             try:
-                desde = datetime.datetime.strptime(desde, '%d/%m/%Y').date()
-                hasta = datetime.datetime.strptime(hasta, '%d/%m/%Y').date()
+                desde = self._get_date(desde, inicio=True)
+                hasta = self._get_date(hasta, inicio=False)
             except ValueError:
                 pass
             else:
@@ -66,3 +70,15 @@ class RangoFechasFiltro(admin.SimpleListFilter):
                 queryset = queryset.filter(**args)
 
         return queryset
+
+    def _get_date(self, fecha, inicio=True):
+        """Devuelve datetime.date o datetime.datetime segun el atributo del modelo"""
+        _fecha = datetime.datetime.strptime(fecha, '%d/%m/%Y')
+        model_field = self.model._meta._name_map[self.value()][0]
+        if isinstance(model_field, DateTimeField):
+            if inicio:
+                return _fecha.replace(hour=0, minute=0, second=0, microsecond=0)
+            else:
+                return _fecha.replace(hour=23, minute=59, second=59, microsecond=999999)
+        else:
+            return _fecha.date()

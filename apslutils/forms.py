dac247084981f42c37b5ac16eb918a39b34dd8f6
Cesc <francesc.arpi@gmail.com>
2014-06-16 12:32:09 +0200

    Para crispy forms, se añade un nuevo MultiField para los Layouts.

--- apslutils/forms.py
+++ apslutils/forms.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 # --------------------------------------------------------------------------
 from django import forms
+from django.template.loader import render_to_string
 
 
 class EntreFechasWidget(forms.MultiWidget):
@@ -51,3 +52,40 @@ class BootstrapMixin(forms.Form):
                 f.widget.attrs['class'] += ' form-control'
             else:
                 f.widget.attrs['class'] = 'form-control'
+
+
+# Si tenemos crispy_forms instalado, definimos un nuevo MultiField compatible
+# con bootstrap3.
+
+try:
+    from crispy_forms.layout import MultiField
+except ImportError:
+    pass
+else:
+    from crispy_forms.layout import TEMPLATE_PACK, Field
+    
+    class MultiFieldBootstrap(MultiField):
+        def __init__(self, label, *fields, **kwargs):
+            super(MultiFieldBootstrap, self).__init__(label, *fields, **kwargs)
+            self.template = "apslutils/multifieldbs.html"
+            
+        def render(self, form, form_style, context, template_pack=TEMPLATE_PACK):
+            requerido, hay_error = False, False
+            
+            campos = []
+            for campo in self.fields:
+                nombre_campo = campo.fields[0] if isinstance(campo, Field) else campo
+                form_campo = form[nombre_campo]
+
+                if isinstance(campo, Field):
+                    form_campo.field.widget.attrs = campo.attrs
+                
+                campos.append(form_campo)
+
+                if form_campo.field.required: requerido = True
+                if form_campo.errors: hay_error = True
+                
+            context.update({"multifield": self, "fields_list": campos,
+                            "is_required": requerido, "has_error": hay_error})
+            
+            return render_to_string(self.template, context)
